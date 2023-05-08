9e4eb1e967797fcd4407c111efdf164e9e28b978
Maik Switalski <maik@zauberzeug.com>
2023-05-08 17:07:28 +0200

    Change logging

--- build.sh
+++ build.sh
@@ -36,7 +36,7 @@ dotnet restore QuickTest.sln || exit 1
 dotnet build --configuration Release QuickTest/QuickTest.csproj || exit 1
 dotnet build --configuration Release Tests/Tests.csproj || exit 1
 
-dotnet test Tests/Tests.csproj  --logger "trx;LogFileName=TestResult.xml"
+dotnet test Tests/Tests.csproj --results-directory .  --logger:"nunit;LogFilePath=TestResult.xml"
 
 createTag
 
